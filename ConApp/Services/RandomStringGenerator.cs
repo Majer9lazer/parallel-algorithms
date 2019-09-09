fdbdac26a86f543d52d17d59f42d3c8d96ca6161
egor sidorenko <sidorenkoegor1999@mail.ru>
2019-09-10 00:25:13 +0600

    Add project files.

---
+++ ConApp/Services/RandomStringGenerator.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConApp.Services
+{
+    public class RandomStringGenerator
+    {
+
+        #region private_readonly_felds
+        private readonly int _stringLength;
+        private readonly IEnumerable<int> _enumerableRange;
+        private readonly ParallelQuery<int> _parallelQuery;
+        private readonly Random _rnd;
+        private readonly StringBuilder _stringBuilder;
+        #endregion
+
+        #region static_fields
+
+        private static StringBuilder _staticBuilder = new StringBuilder();
+        private static readonly Random StaticRnd = new Random();
+        #endregion
+        public RandomStringGenerator(int stringLength)
+        {
+            _stringLength = stringLength;
+            _stringBuilder = new StringBuilder(stringLength);
+            _enumerableRange = Enumerable.Range(0, stringLength);
+            _parallelQuery = ParallelEnumerable.Range(0, stringLength);
+            _rnd = new Random();
+        }
+
+        public string Generate()
+        {
+            foreach (var i in _enumerableRange)
+            {
+                _stringBuilder.Append((char)_rnd.Next(127 + 1));
+            }
+
+            var result = _stringBuilder.ToString();
+
+            _stringBuilder.Clear();
+
+            return result;
+        }
+
+        public string GenerateParallel()
+        {
+            Parallel.ForEach(_parallelQuery, (item) =>
+            {
+                _stringBuilder.Append((char)StaticRnd.Next(127 + 1));
+            });
+
+            var result = _stringBuilder.ToString();
+
+            _stringBuilder.Clear();
+
+            return result;
+        }
+
+        public static string Generate(int stringLen)
+        {
+            var enumRange = Enumerable.Range(0, stringLen);
+
+
+            foreach (var unused in enumRange)
+            {
+                _staticBuilder.Append((char)StaticRnd.Next(127 + 1));
+            }
+
+            var result = _staticBuilder.ToString();
+            _staticBuilder.Clear();
+
+            return result;
+        }
+
+        public static string GenerateParallel(int stringLen)
+        {
+            _staticBuilder = new StringBuilder(stringLen);
+
+            var plEnum = ParallelEnumerable
+            .Range(0, stringLen);
+
+            Parallel.ForEach(plEnum, (item) => { _staticBuilder.Append((char)StaticRnd.Next(127 + 1)); });
+
+            var result = _staticBuilder.ToString();
+            _staticBuilder.Clear();
+
+            return result;
+        }
+
+    }
+}
---
+++ ConAppTests/RandomStringGeneratorTests.cs
@@ -0,0 +1,136 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using ConApp.Services;
+using NUnit.Framework;
+
+namespace ConAppTests
+{
+    public class RandomStringGeneratorTests
+    {
+        private RandomStringGenerator _stringGenerator;
+
+       
+        [SetUp]
+        public void Setup()
+        {
+            _stringGenerator = new RandomStringGenerator(1_000_000);
+        }
+
+        [Test]
+        public void InitialTest()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Generate_test()
+        {
+            var generateStr = _stringGenerator.Generate();
+
+            Assert.IsNotNull(generateStr);
+        }
+
+        [Test]
+        public void GenerateParallel_test()
+        {
+            var generatedStr = _stringGenerator.GenerateParallel();
+
+            Assert.IsNotNull(generatedStr);
+        }
+
+        [Test]
+        public void Benchmark()
+        {
+            var elapsedInfoTimes = new Dictionary<string, TimeSpan>();
+
+            var timer = Stopwatch.StartNew();
+
+            var inlineGeneratedStr = _stringGenerator.Generate();
+
+            timer.Stop();
+            elapsedInfoTimes.Add(nameof(_stringGenerator.Generate),timer.Elapsed);
+
+            timer.Restart();
+            var parallelGeneratedStr = _stringGenerator.GenerateParallel();
+
+            timer.Stop();
+            elapsedInfoTimes.Add(nameof(_stringGenerator.GenerateParallel), timer.Elapsed);
+
+            foreach (var infoTime in elapsedInfoTimes)
+            {
+                Console.WriteLine($"\"{infoTime.Key}\" elapsed = {infoTime.Value.TotalMilliseconds} ms, {infoTime.Value.TotalSeconds} s");
+            }
+
+            Assert.IsNotNull(inlineGeneratedStr);
+            Assert.IsNotNull(parallelGeneratedStr);
+            Assert.AreNotEqual(inlineGeneratedStr,parallelGeneratedStr);
+        }
+
+        [Test]
+        [TestCase(10_000)]
+        [TestCase(100_000)]
+        [TestCase(1_000_000)]
+        [TestCase(10_000_000)]
+        [TestCase(100_000_000)]
+        public void Benchmark_static_generation(int stringLen)
+        {
+            var elapsedInfoTimes = new Dictionary<string, TimeSpan>();
+
+            var timer = Stopwatch.StartNew();
+
+            var inlineGeneratedStr = RandomStringGenerator.Generate(stringLen);
+
+            timer.Stop();
+            elapsedInfoTimes.Add(nameof(RandomStringGenerator.Generate), timer.Elapsed);
+
+            timer.Restart();
+            var parallelGeneratedStr = RandomStringGenerator.GenerateParallel(stringLen);
+
+            timer.Stop();
+            elapsedInfoTimes.Add(nameof(RandomStringGenerator.GenerateParallel), timer.Elapsed);
+
+            foreach (var infoTime in elapsedInfoTimes)
+            {
+                Console.WriteLine($"\"{infoTime.Key}\" elapsed = {infoTime.Value.TotalMilliseconds} ms, {infoTime.Value.TotalSeconds} s");
+            }
+
+            Assert.IsNotNull(inlineGeneratedStr);
+            Assert.IsNotNull(parallelGeneratedStr);
+            Assert.AreNotEqual(inlineGeneratedStr, parallelGeneratedStr);
+        }
+
+        [Test]
+        [TestCase(10_000)]
+        [TestCase(100_000)]
+        [TestCase(1_000_000)]
+        [TestCase(10_000_000)]
+        [TestCase(100_000_000)]
+        public void Benchmark_static_generation_reverse(int stringLen)
+        {
+            var elapsedInfoTimes = new Dictionary<string, TimeSpan>();
+
+            var timer = Stopwatch.StartNew();
+
+            var parallelGeneratedStr = RandomStringGenerator.GenerateParallel(stringLen);
+
+            timer.Stop();
+            elapsedInfoTimes.Add(nameof(RandomStringGenerator.GenerateParallel), timer.Elapsed);
+
+            timer.Restart();
+            var inlineGeneratedStr = RandomStringGenerator.Generate(stringLen);
+
+            timer.Stop();
+            elapsedInfoTimes.Add(nameof(RandomStringGenerator.Generate), timer.Elapsed);
+
+            foreach (var infoTime in elapsedInfoTimes)
+            {
+                Console.WriteLine($"\"{infoTime.Key}\" elapsed = {infoTime.Value.TotalMilliseconds} ms, {infoTime.Value.TotalSeconds} s");
+            }
+
+            Assert.IsNotNull(inlineGeneratedStr);
+            Assert.IsNotNull(parallelGeneratedStr);
+            Assert.AreNotEqual(inlineGeneratedStr, parallelGeneratedStr);
+        }
+    }
+}
